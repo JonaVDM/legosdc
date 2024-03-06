d24ec32ff3a5330b0ba39af39f4a4c904abef9a4
Johan Beimers <johan.beime@gmail.com>
2024-03-06 20:31:33 +0100

    test

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import Font
+from time import sleep
+
 
 # Get sensor instances
 colour_sensor = ColorSensor(Port.S2)
@@ -99,51 +101,136 @@ def main():
     output_text("Starting dancing...")
     brick.speaker.say("Dancing the Self Driving Challenge Dance!")
     brick.speaker.play_notes(['E4/4', 'C4/4', 'D4/4', 'E4/4', 'D4/4', 'C4/4', 'C4/4', 'E4/4', 'B4/4', 'B4/4', 'A4/4', 'A4/4'], tempo=240)
-    while (True):
-        steer.run_target(steer_speed // 2, max_left_angle // 2)
-        steer.run_target(steer_speed // 2, max_right_angle // 2)
-        steer.run_target(steer_speed // 2, 0)
 
-        steer.run_target(steer_speed * 2, max_left_angle // 2)
-        steer.run_target(steer_speed * 2, max_right_angle // 2)
-        steer.run_target(steer_speed * 2, 0)
+def drive():
+    pv = 55
+    kp = 0.55
+    ki = 0.0003
+    kd = 2.5
+    dt = 0.1
+    integral = 0.0
+    pre_error = 0.0
 
-        steer.run_target(steer_speed * 2, max_left_angle // 2)
-        steer.run_target(steer_speed * 2, max_right_angle // 2)
-        steer.run_target(steer_speed * 2, 0)
+    brick.screen.clear()
+    brick.screen.set_font(Font(size=12))
 
-        engine.straight(100)
-        engine.straight(-100)
+    # Some vehicle settings
+    wheel_diameter_mm = 40
+    wheel_track_mm = 120
+    engine_acceleration = 250
+    engine_speed = 200
+    steer_speed = 250
 
-        engine.straight(50)
-        engine.straight(-50)
+    output_text("Homing steering wheels...")
+    max_left_angle, max_right_angle = home_steer(steer)
+    output_text("Done!")
 
-        engine.straight(50)
-        engine.straight(-50)
+    # Prepare the engine by combining both motors
+    output_text("Configuring drivetrain...")
+    engine = DriveBase(left_wheel, right_wheel, wheel_diameter_mm, wheel_track_mm)
+    engine.settings(straight_speed=engine_speed, straight_acceleration=engine_acceleration, turn_rate=0, turn_acceleration=0)
+    output_text("Done!")
 
-        steer.run_target(steer_speed, max_left_angle)
-        engine.straight(100)
-        steer.run_target(steer_speed, max_right_angle)
-        engine.straight(100)
-        steer.run_target(steer_speed, 0)
+    while (not bumper_sensor.pressed()):
+        wait(100)
+        
+    # Sound a beep, which is required for the competition to allow accurate lap-timing.
+    brick.speaker.beep(500, 250)
+    # Drive one centimeter
+    output_text("GO!")
 
-        steer.run_target(steer_speed, max_left_angle)
-        engine.straight(-100)
-        steer.run_target(steer_speed, max_right_angle)
-        engine.straight(-100)
-        steer.run_target(steer_speed, 0)
+    left_wheel.run(180)
+    right_wheel.run(180)
 
-        steer.run_target(steer_speed, max_right_angle)
-        engine.straight(100)
-        steer.run_target(steer_speed, max_left_angle)
-        engine.straight(100)
-        steer.run_target(steer_speed, 0)
+    while True:
+        #engine.straight(60)
+        set_point = infrared_sensor.distance()
+        #output_text(str(set_point))
+        error = set_point - pv
 
-        steer.run_target(steer_speed, max_right_angle)
-        engine.straight(-100)
-        steer.run_target(steer_speed, max_left_angle)
-        engine.straight(-100)
-        steer.run_target(steer_speed, 0)
+        proportional_out = error * kp
+        
+        integral += error * dt
+        integral_out = integral * ki
 
+        derivative = (error - pre_error) / dt
+        derivative_out = derivative * kd
+
+        output = proportional_out + integral_out + derivative_out
+        pre_error = error
+
+        angle = max_left_angle + (max_right_angle - max_left_angle) * output / 100.0
+
+        steer.track_target(output)
+        wait(dt * 1000)
+
+        output_text(integral)
+
+        #if set_point < 10:
+            #engine.straight(-40)
+
+def drive2():
+    brick.screen.clear()
+    brick.screen.set_font(Font(size=12))
+
+    # Some vehicle settings
+    wheel_diameter_mm = 40
+    wheel_track_mm = 120
+    engine_acceleration = 250
+    engine_speed = 200
+    steer_speed = 250
+
+    output_text("Homing steering wheels...")
+    max_left_angle, max_right_angle = home_steer(steer)
+    output_text("Done!")
+
+    # Prepare the engine by combining both motors
+    output_text("Configuring drivetrain...")
+    engine = DriveBase(left_wheel, right_wheel, wheel_diameter_mm, wheel_track_mm)
+    engine.settings(straight_speed=engine_speed, straight_acceleration=engine_acceleration, turn_rate=0, turn_acceleration=0)
+    output_text("Done!")
+
+    while (not bumper_sensor.pressed()):
+        wait(100)
+        
+    # Sound a beep, which is required for the competition to allow accurate lap-timing.
+    brick.speaker.beep(500, 250)
+    # Drive one centimeter
+    output_text("GO!")
+
+    last_distance_2 = 0 
+    while True: 
+        engine.straight(40)
+        distance_2 = infrared_sensor.distance()
+        
+        if distance_2 > last_distance_2:
+            1+1
+        else:
+            if angle == 60:
+                angle = -60
+            if angle == -60:
+                angle = 60
+
+        steer.track_target(angle)
+        last_distance_2 = distance_2
+
+        if distance_2 < 30:
+            engine.straight(-40)
+
+def play_ride_of_the_valkyries():
+    output_text("Starting to ride...")
+    brick.speaker.say("Time to ride")
+    brick.speaker.play_notes(["E4/4", "E4/4", "E4/4", "C4/4", "G4/4", "G4/4", "G4/4", "E4/4", "E4/4", "E4/4", "B3/4", "D4/4", "E4/4",
+                              "E4/4", "E4/4", "C4/4", "G4/4", "G4/4", "G4/4", "E4/4", "E4/4", "E4/4", "B3/4", "D4/4", "E4/4",
+                              "E4/4", "E4/4", "C4/4", "G4/4", "G4/4", "G4/4", "E4/4", "E4/4", "E4/4", "B3/4", "D4/4", "E4/4",
+                              "F4/4", "F4/4", "F4/4", "A4/4", "C5/4", "A4/4", "F4/4", "E4/4", "E4/4", "E4/4", "D4/4", "D4/4", "D4/4",
+                              "D4/4", "D4/4", "E4/4", "F4/4", "E4/4", "F4/4", "G4/4", "A4/4", "B4/4", "C5/4", "D5/4", "E5/4", "F5/4",
+                              "G5/4", "A5/4", "G5/4", "F5/4", "E5/4", "D5/4", "C5/4", "B4/4", "A4/4", "G4/4", "F4/4", "E4/4", "D4/4",
+                              "C4/4", "B3/4", "A3/4", "G3/4", "F3/4", "E3/4", "D3/4", "C3/4", "B2/4", "A2/4"], tempo=480)
+            # sound.tonenot/], 400)
+        # sleep(note[1] * 0.4)
+
+        
 if __name__ == '__main__':
-    main()
+    play_ride_of_the_valkyries()
+    drive2()
